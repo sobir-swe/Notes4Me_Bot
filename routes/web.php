571d87caaf4646e6a570197e33ec4a769ab5e208
sob1rdev <20sobirjon05@gmail.com>
2024-10-17 10:39:13 +0500

    feat: update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('setwebhook', function () {
+    $response = Telegram::setWebhook(['url' => 'https://a9be-89-236-218-41.ngrok-free.app/api/telegram/webhook']);
 });
